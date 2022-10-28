58cd5d3bc6105cf4ffe467576c0a9b46ab15a6c2
Rodrigo Bossini <professorbossini@gmail.com>
2022-10-28 11:00:05 -0300

    endpoint para cadastro de usuario implementado

--- backend.js
+++ backend.js
@@ -1,36 +1,61 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const mongoose = require ('mongoose')
+const uniqueValidator = require ('mongoose-unique-validator')
 // aplicar um middleware
 app.use(express.json())
 //aplicamos mais um middleware
 app.use(cors())
 
+async function conectarAoMongoDB() {
+await
+mongoose.connect(`mongodb+srv://professorbossini:professorbossini@cluster0.wttmkyk.mongodb.net/?retryWrites=true&w=majority`)
+}
+
+const Filme = mongoose.model("Filme", mongoose.Schema({
+  titulo: {type: String},
+  sinopse: {type: String}
+}))
+
+const usuarioSchema = mongoose.Schema({
+  login: {type: String, required: true, unique: true},
+  password: {type: String, required: true}
+})
+usuarioSchema.plugin(uniqueValidator)
+const Usuario = mongoose.model("Usuario", usuarioSchema)
 
 // let e const
-let filmes = [
-  {
-    titulo: 'Forrest Gump - O Contador de Histórias', 
-    sinopse: 'Quarenta anos da história dos Estados Unidos, vistos pelos olhos de Forrest Gump (Tom Hanks), um rapaz com QI abaixo da média e boas intenções.'
-  },
-  {
-    titulo: 'Um Sonho de Liberdade',
-    sinopse: '"Em 1946, Andy Dufresne (Tim Robbins), um jovem e bem sucedido banqueiro, tem a sua vida radicalmente modificada ao ser condenado por um crime que nunca cometeu, o homicídio de sua esposa e o amante dela'
-  }
-]
+// let filmes = [
+//   {
+//     titulo: 'Forrest Gump - O Contador de Histórias', 
+//     sinopse: 'Quarenta anos da história dos Estados Unidos, vistos pelos olhos de Forrest Gump (Tom Hanks), um rapaz com QI abaixo da média e boas intenções.'
+//   },
+//   {
+//     titulo: 'Um Sonho de Liberdade',
+//     sinopse: '"Em 1946, Andy Dufresne (Tim Robbins), um jovem e bem sucedido banqueiro, tem a sua vida radicalmente modificada ao ser condenado por um crime que nunca cometeu, o homicídio de sua esposa e o amante dela'
+//   }
+// ]
 
 //POST/////// --sa
 //http://localhost:3000/filmes
-app.post('/filmes', (req, res) => {
+app.post('/filmes', async (req, res) => {
   //pegar o titulo existente na requisição
   const titulo = req.body.titulo
   //pegar a sinopse existente na requisição
   const sinopse = req.body.sinopse
   //construir um objeto JSON que representa um filme
   // const filme = {titulo: titulo, sinopse: sinopse}
-  const filme = {titulo, sinopse}
+
+  const filme = new Filme({ titulo, sinopse })
+
+  await filme.save()
+
+  const filmes = await Filme.find()
+
+  // const filme = {titulo, sinopse}
   //adicionar o novo filme à coleção filmes
-  filmes.push(filme)
+  // filmes.push(filme)
   //responder ao cliente entregando a ele uma cópia da coleção de filmes atualizada
   res.json(filmes)  
 })
@@ -39,18 +64,33 @@ app.post('/filmes', (req, res) => {
 //GET
 //http://localhost:3000/filmes
 //eis a lista de filmes..
-app.get('/filmes', (req, res) => {
+app.get('/filmes', async (req, res) => {
+  const filmes = await Filme.find()
   res.json(filmes)
 })
 
-
-// http://localhost:3000/hey
-//hey
-app.get('/hey', (req, res) => {
-  res.send('hey')
+app.post('/signup', async (req, res) => {
+  //1. extrair login e senha da req
+  const login = req.body.login
+  const password = req.body.password
+  //2. construir um objeto Usuario usando o modelo da Mongoose
+  const usuario = new Usuario({login, password})
+  //3. chamar o método save sobre o objeto construtor
+  const respMongo = await usuario.save()
+  console.log(respMongo)
+  //4. encerrar o tratamento da requisição
+  res.end()
 })
 
 // arrow function
-app.listen(3000, () => {console.log("Servidor em funcionamento...")})
+app.listen(3000, () => {
+  try{
+    conectarAoMongoDB()
+    console.log("Servidor em funcionamento...")
+  }
+  catch (e){
+    console.log("Erro", e)
+  }
+})
 
 
