de36a561e8be92c0e9f4cafcc47cf11db494f4e4
Rodrigo Bossini <professorbossini@gmail.com>
2022-11-16 11:04:41 -0300

    apostila01(html_js_mongodb): codigo 2.23.11 feito
    modal para cadastro pronto
    funcao exibirAlerta generica implementada
    
    retomar a partir do codigo 2.23.12

--- front/js/frontend.js
+++ front/js/frontend.js
@@ -8,38 +8,38 @@ async function obterFilmes() {
   const filmes = (await axios.get(urlCompleta)).data
   let tabela = document.querySelector('.filmes')
   let corpoTabela = tabela.getElementsByTagName('tbody')[0]
-  for (let filme of filmes){
+  for (let filme of filmes) {
     let linha = corpoTabela.insertRow()
-    let celulaTitulo = linha.insertCell(0)  
+    let celulaTitulo = linha.insertCell(0)
     let celulaSinopse = linha.insertCell(1)
     celulaTitulo.innerHTML = filme.titulo
     celulaSinopse.innerHTML = filme.sinopse
   }
 }
 
-async function cadastrarFilme(){
+async function cadastrarFilme() {
   const urlCompleta = `${protocolo}://${baseURL}${filmesEndpoint}`
   let tituloInput = document.querySelector('#tituloInput')
   let sinopseInput = document.querySelector('#sinopseInput')
   let titulo = tituloInput.value
   let sinopse = sinopseInput.value
-  if (titulo && sinopse){
+  if (titulo && sinopse) {
     tituloInput.value = ''
     sinopseInput.value = ''
     // const filme = {titulo, sinopse}
-    const filmes = (await axios.post(urlCompleta, {titulo, sinopse})).data
+    const filmes = (await axios.post(urlCompleta, { titulo, sinopse })).data
     let tabela = document.querySelector('.filmes')
     let corpoTabela = tabela.getElementsByTagName('tbody')[0]
     corpoTabela.innerHTML = ''
-    for (filme of filmes){
+    for (filme of filmes) {
       let linha = corpoTabela.insertRow(0)
       let celulaTitulo = linha.insertCell(0)
       let celulaSinopse = linha.insertCell(1)
       celulaTitulo.innerHTML = filme.titulo
-      celulaSinopse.innerHTML = filme.sinopse  
+      celulaSinopse.innerHTML = filme.sinopse
     }
   }
-  else{
+  else {
     let alert = document.querySelector('.alert')
     alert.classList.add('show')
     alert.classList.remove('d-none')
@@ -52,4 +52,40 @@ async function cadastrarFilme(){
 }
 
 async function cadastrarUsuario() {
+  const usuarioCadastroInput = document.querySelector('#usuarioCadastroInput')
+  const passwordCadastroInput = document.querySelector('#passwordCadastroInput')
+  const usuarioCadastro = usuarioCadastroInput.value
+  const passwordCadastro = passwordCadastroInput.value
+  if (usuarioCadastro && passwordCadastro) {
+    //já já faremos essa parte
+    try {
+      const URLCompleta = `${protocolo}://${baseURL}${cadastroUsuarioEndpoint}`
+      await axios.post(
+        URLCompleta,
+        { login: usuarioCadastro, password: passwordCadastro }
+      )
+      usuarioCadastroInput.value = ""
+      passwordCadastroInput.value = ""
+      exibirAlerta('.alert-modal-cadastro', "Usuário cadastrado com sucesso", ['show', 'alert-success'], ['d-none'], 2000)
+    }
+    catch (erro) {
+      exibirAlerta('.alert-modal-cadastro', "Erro ao cadastrar usuário", ['show', 'alert-danger'], ['d-none'], 2000)
+    }
+  }
+  else {
+    exibirAlerta('.alert-modal-cadastro', "Preencha todos os campos", ['show', 'alert-danger'], ['d-none'], 2000)
+  }
+}
+
+function exibirAlerta(seletor, conteudo, classesAAdicionar, classeARemover, timeout) {
+  //exibir um alerta instruindo o usuário a preencher todos os campos
+  let alert = document.querySelector(seletor)
+  alert.innerHTML = conteudo
+  // alert.classList.add('show', 'alert-danger')
+  alert.classList.add(...classesAAdicionar)
+  alert.classList.remove(...classeARemover)
+  setTimeout(() => {
+    alert.classList.remove('show')
+    alert.classList.add('d-none')
+  }, timeout)
 }
